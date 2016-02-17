735a0ac5ac011aad4d8a85d0efad48200af330c0
rwitzel <rodrigo.witzel@zalando.de>
2016-02-17 11:20:34 +0100

    restored the original version

--- src/main/java/demos/Application.java
+++ src/main/java/demos/Application.java
@@ -3,45 +3,24 @@ package demos;
 import org.apache.camel.CamelContext;
 import org.apache.camel.builder.RouteBuilder;
 import org.apache.camel.impl.DefaultCamelContext;
-import org.apache.camel.spi.Registry;
-import org.apache.camel.spring.spi.ApplicationContextRegistry;
-import org.apache.ftpserver.FtpServer;
-import org.springframework.context.ApplicationContext;
-import org.springframework.context.annotation.AnnotationConfigApplicationContext;
-
-import static demos.FtpServerCreator.embeddedFtpServer;
-import static org.apache.camel.model.dataformat.BindyType.Csv;
 
 public class Application {
 
     public static void main(String args[]) throws Exception {
 
-        FtpServer ftpServer = embeddedFtpServer(21000, Application.class.getResource("/users.properties"));
-        ftpServer.start();
-
-        ApplicationContext applicationContext = new AnnotationConfigApplicationContext(SpringConfig.class);
-        Registry registry = new ApplicationContextRegistry(applicationContext);
-
-        CamelContext context = new DefaultCamelContext(registry);
+        CamelContext context = new DefaultCamelContext();
 
         context.addRoutes(new RouteBuilder() {
             public void configure() {
-                from("ftp:rider:secret@localhost:21000/data/inbox?noop=true&connectTimeout=5000&timeout=5000")
-                        .routeId("travelStockImport")
-                        .unmarshal().bindy(Csv, "demos")
-                        .to("mustache:/updateArticleWeights.sql").setBody(body().regexReplaceAll(",(\\s+;)", "$1"))
-                        .log("log sql: ${body}")
-                        .to("jdbc:release_ias_db")
-                ;
+                from("file:data/inbox?noop=true")
+                        .to("file:data/outbox");
             }
         });
 
         context.start();
-        Thread.sleep(2000);
+        Thread.sleep(10000);
 
         context.stop();
-
-        ftpServer.stop();
     }
 
 }
--- src/main/resources/createDb.sql
+++
@@ -1,1 +0,0 @@
-CREATE TABLE ias_article_weight (sku VARCHAR(255), warehouse VARCHAR(255), weight DECIMAL );
