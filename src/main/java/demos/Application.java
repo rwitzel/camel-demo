061d98388a4e65f1ba7ea8cf6c375b7f148772cb
rwitzel <rodrigo.witzel@zalando.de>
2016-03-08 18:19:51 +0100

    split and aggregate

--- src/main/java/demos/Application.java
+++ src/main/java/demos/Application.java
@@ -1,8 +1,13 @@
 package demos;
 
 import org.apache.camel.CamelContext;
+import org.apache.camel.ConsumerTemplate;
+import org.apache.camel.Exchange;
+import org.apache.camel.ProducerTemplate;
 import org.apache.camel.builder.RouteBuilder;
 import org.apache.camel.impl.DefaultCamelContext;
+
+import static java.util.Arrays.asList;
 
 public class Application {
 
@@ -12,13 +17,23 @@ public class Application {
 
         context.addRoutes(new RouteBuilder() {
             public void configure() {
-                from("file:data/inbox?noop=true")
-                        .to("file:data/outbox");
+                from("direct:start")
+                        .routeId("playground")
+                        .split(body())
+                        .log("body: ${body}")
+                        .end()
+                        .to("seda:end");
             }
         });
 
         context.start();
-        Thread.sleep(10000);
+
+        ProducerTemplate producer = context.createProducerTemplate();
+        producer.sendBody("direct:start", asList("abc", "def"));
+
+        ConsumerTemplate consumer = context.createConsumerTemplate();
+        Exchange received = consumer.receive("seda:end");
+        System.out.println("received.getIn(): " + received.getIn());
 
         context.stop();
     }
